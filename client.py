166fb9c4eec12f0a1398558ee7483d7bae1694b1
Peter Santos <AIMEPTSTSAM@mail.com>
2022-06-21 20:12:09 +0200

    Update client server

--- client.py
+++ client.py
@@ -3,15 +3,56 @@ import requests, json
 from preprocess import preprocess_pipeline
 
 url = 'http://127.0.0.1:5000/'
+"""
+/predict: POST {'feature1': 'value1', 'feature2': 'value2', ...} (must be logged in to predict) Response prediction
+/register: POST {'username': ..., 'password': ***} Response status (success, already exists or error)
+/login: POST {'username': ..., 'password': ***} Response status (logged in, wrong or error)
+/predict_no_login: POST {'feature1': 'value1', 'feature2': 'value2', ...} (debug, no login needed) Response prediction
+"""
+
+
+def register(username, password):
+    response = requests.post(
+        url + 'register',
+        json.dumps({'username': username, 'password': password})
+    )
+    print(response.text)
+
+
+def login(username, password):
+    response = requests.post(
+        url + 'login',
+        json.dumps({'username': username, 'password': password})
+    )
+    print(response.text)
+
+
+def predict(features, need_login=True):
+    for feature in features:
+        data = json.dumps(list(feature))
+        if need_login:
+            response = requests.post(url + 'predict', data)
+        else:
+            response = requests.post(url + 'predict_no_login', data)
+        print(response, response.text)
+
 
 if __name__ == '__main__':
+    # response = requests.post(
+    #     url + 'register',
+    #     json.dumps({'username': 'newuser', 'password': 'newpassword'})
+    # )
+    # print(response.text)
+    #
+    # exit()
+
+
+    # login('abc', 'abc')
+    # exit()
     path = 'data/car_loan_trainset.csv'
     model_save_path = 'saved/model_no_id'
-    df = pd.read_csv(path).tail(100)
+    df = pd.read_csv(path).tail(3)
 
     df, x_columns = preprocess_pipeline(df)
     features = df[x_columns].to_numpy()
-    for row in features:
-        data = json.dumps(list(row))
-        response = requests.post(url, data)
-        print(response, response.text)
+    predict(features, need_login=False)
--- server.py
+++ server.py
@@ -1,49 +1,115 @@
 import json
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 import pandas as pd
 from preprocess import preprocess_pipeline
 from predict import load_model, predict
 from database import connect
 import psycopg2
+from flask_login import current_user, login_user, UserMixin, LoginManager
 
 
 app = Flask(__name__)
+app.secret_key = 'abc'
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-@app.route('/', methods=['POST'])
+
+@app.route('/predict', methods=['POST'])
 def make_prediction():
+    if current_user.is_authenticated:
+        features = json.loads(request.data)
+        prediction = predict(model, features)
+        submit_prediction(prediction, features)
+        return jsonify(prediction)
+    return Response('Please login first!', 401)
+
+
+@app.route('/predict_no_login', methods=['POST'])
+def make_prediction_no_login():
     features = json.loads(request.data)
     prediction = predict(model, features)
+    submit_prediction(prediction, features)
     return jsonify(prediction)
 
 
 @app.route('/register', methods=['POST'])
 def register():
     data = json.loads(request.data)
-    sql = f"""SELECT * FROM users WHERE username = {data['username']}"""
+    sql = f"""SELECT * FROM users WHERE username = '{data['username']}';"""
 
     cursor = connection.cursor()
-    response = -1
     try:
         cursor.execute(sql)
         connection.commit()
         if len(cursor.fetchall()):
-            response = -1
+            return Response('Username already exists, choose a different one!', 401)
         else:
-            response = 0
+            sql = f"""INSERT INTO users (username, password) VALUES (
+              '{data['username']}',
+              crypt('{data['password']}', gen_salt('bf'))
+            );"""
+            try:
+                cursor.execute(sql)
+                connection.commit()
+            except (Exception, psycopg2.DatabaseError) as error:
+                print(error)
+                return Response('Sorry, server error.', 404)
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
-
-    sql = f"""INSERT INTO users (username, password) VALUES (
-      '{data['username']}',
-      crypt('{data['password']}', gen_salt('bf'))
-    );"""
-    try:
-        cursor.execute(sql)
-        connection.commit()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
+        return Response('Sorry, server error.', 404)
     cursor.close()
-    return jsonify(response)
+    return Response('User registration successful!', 200)
+
+
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if current_user.is_authenticated:
+        return Response('Already logged in.', 200)
+    if request.method == 'POST':
+        data = json.loads(request.data)
+        cursor = connection.cursor()
+        sql = f"""SELECT * FROM users WHERE username = '{data['username']}'
+        AND password = crypt('{data['password']}', password);"""
+
+        try:
+            cursor.execute(sql)
+            connection.commit()
+            if len(cursor.fetchall()):
+                login_user(User(data['username'], hash=None))
+                return Response('Logged in.', 200)
+            else:
+                return Response('Wrong user credentials', 401)
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+            return Response('Sorry, server error.', 404)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id, None)
+
+
+def submit_prediction(prediction, features, customer_id=None):
+    cursor = connection.cursor()
+    if customer_id is not None:
+        sql = f"""INSERT INTO user_prediction (customer_id, prediction, features, time_predicted)
+        VALUES ({customer_id}, {prediction}, '{{{', '.join(features)}}}', now())"""
+    else:
+        sql = f"""INSERT INTO user_prediction (prediction, features, time_predicted)
+        VALUES ({prediction}, '{{{', '.join([str(x) for x in features])}}}', now())"""
+
+    cursor.execute(sql)
+    connection.commit()
+    cursor.close()
+
+
+class User(UserMixin):
+    def __init__(self, username, hash):
+        self.username = username
+
+    @property
+    def id(self):
+        return self.username
 
 
 if __name__ == '__main__':
@@ -56,4 +122,5 @@ if __name__ == '__main__':
     df, x_columns = preprocess_pipeline(df)
 
     model, optimizer = load_model(model_save_path, len(x_columns), lr=0.000001)
+
     app.run(debug=True)
